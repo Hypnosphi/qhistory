1b5dc78b50e99a8cedc0260fb0485c58afb2397c
Paul Sherman <paul.sherman.88@gmail.com>
2017-02-11 00:19:30 -0600

    tests

---
+++ tests/qhistory.spec.js
@@ -0,0 +1,125 @@
+import qhistory from '../src/index'
+import { createMemoryHistory } from 'history'
+import { stringify, parse } from 'qs'
+
+describe('qhistory', () => {
+  describe('query', () => {
+    it('will be present on initial location', () => {
+      const history = createMemoryHistory({
+        initialEntries: [{ pathname: '/first', search: '?second=third'}]
+      })
+      const q = qhistory(history, stringify, parse)
+      expect(q.location.query).toBeDefined()
+    })
+
+    it('will be present after navigating', () => {
+      const history = createMemoryHistory()
+      const q = qhistory(history, stringify, parse)
+
+      q.listen((location) => {
+        expect(location.query).toEqual({ tooGoodToBe: 'true' })
+        expect(location.search).toBe('?tooGoodToBe=true')
+      })
+
+      q.push('/test?tooGoodToBe=true')
+    })
+
+    it('will be an empty query object when there is no query/search', () => {
+      const history = createMemoryHistory()
+      const q = qhistory(history, stringify, parse)
+
+      q.listen((location) => {
+        expect(location.query).toBeInstanceOf(Object)
+        expect(Object.keys(location.query).length).toBe(0)
+      })
+
+      q.push('/test')
+    })
+  })
+
+  describe('search', () => {
+    it('will prefer query object over search string', () => {
+      const history = createMemoryHistory()
+      const q = qhistory(history, stringify, parse)
+
+      q.push({ pathname: '/somewhere',
+        search: '?overRainbow=false',
+        query: { overRainbow: true }
+      })
+      const mostRecentLocation = q.entries[q.entries.length-1]
+      expect(mostRecentLocation.search).toBe('?overRainbow=true')
+    })
+
+    it('uses search if no query provided', () => {
+      const history = createMemoryHistory()
+      const q = qhistory(history, stringify, parse)
+      q.push({ pathname: '/somewhere', search: '?overRainbow=false' })
+      const mostRecentLocation = q.entries[q.entries.length-1]
+      expect(mostRecentLocation.search).toBe('?overRainbow=false')
+    })
+
+    it('sets search to empty string if no query/search', () => {
+      const history = createMemoryHistory()
+      const q = qhistory(history, stringify, parse)
+      q.push({ pathname: '/somewhere' })
+      const mostRecentLocation = q.entries[q.entries.length-1]
+      expect(mostRecentLocation.search).toBe('')
+    })
+  })
+
+  describe('push', () => {
+    it('will set search string for location passed to actual history', () => {
+      const history = createMemoryHistory()
+      const q = qhistory(history, stringify, parse)
+
+      q.push({ pathname: '/somewhere', query: { overRainbow: true }})
+      const mostRecentLocation = q.entries[q.entries.length-1]
+      expect(mostRecentLocation.search).toBe('?overRainbow=true')
+    })
+
+    it('will call the history instance\'s push method', () => {
+      const history = createMemoryHistory()
+      const q = qhistory(history, stringify, parse)
+
+      q.push({ pathname: '/somewhere', query: { overRainbow: true }})
+      expect(q.action).toBe('PUSH')
+    })
+  })
+
+  describe('replace', () => {
+    it('will set search string for location passed to actual history', () => {
+      const history = createMemoryHistory()
+      const q = qhistory(history, stringify, parse)
+
+      expect(q.length).toBe(1)
+      q.replace({ pathname: '/somewhere', query: { overRainbow: true }})
+      const mostRecentLocation = q.entries[q.entries.length-1]
+      expect(mostRecentLocation.search).toBe('?overRainbow=true')
+    })
+
+    it('will call the history instance\'s replace method', () => {
+      const history = createMemoryHistory()
+      const q = qhistory(history, stringify, parse)
+
+      expect(q.length).toBe(1)
+      q.replace({ pathname: '/somewhere', query: { overRainbow: true }})
+      // test length to ensure replace was called
+      expect(q.length).toBe(1)
+      expect(q.action).toBe('REPLACE')
+    })
+  })
+
+  describe('createHref', () => {
+    it('uses the query object when creating a path from location', () => {
+      const history = createMemoryHistory()
+      const q = qhistory(history, stringify, parse)
+      const location = {
+        pathname: '/neighborhood',
+        query: {
+          beautifulDay: true
+        }
+      }
+      expect(q.createHref(location)).toBe('/neighborhood?beautifulDay=true')
+    })
+  })
+})
