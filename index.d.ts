2f625ea89f0b3ffc95b6af7a26b463f06ccc3afb
Vašek Henzl <vasek.henzl@gmail.com>
2018-12-06 19:08:22 +1300

    Fix TypeScript declarations (#4)

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,6 @@ import { History } from 'history';
 
 type Stringify = (query: any) => string;
 type Parse = (query: string) => any;
-const historyWithQuery: (history: History, stringify: Stringify, parse: Parse) => History;
+declare const historyWithQuery: (history: History, stringify: Stringify, parse: Parse) => History;
 
 export default historyWithQuery;
